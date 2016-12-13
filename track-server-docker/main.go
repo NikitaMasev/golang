d20eb466f733aba19f4043c9d285ea36034e8e4e
Nikita <Nikita.Masev@gmail.com>
2016-12-13 10:44:59 -0800

    test_docker

--- track-server-docker/main.go
+++ track-server-docker/main.go
@@ -22,7 +22,7 @@ func main() {
 /* Instruction for docker
 
 1. Run Consul : $ ./consul agent -server -bind 127.0.0.1 -ui-dir ~/consul/ui/ -data-dir ~/consul/data/
-2. Build Docker container: $ sudo docker build -t app .
-3. Run Docker container: $ sudo docker run -e CONSUL_HTTP_ADDR=8500 -p 8085:8085 app
+2. Build Docker container: $ sudo docker build -t golang-latest .
+3. Run Docker container: $ sudo docker run -e CONSUL_HTTP_ADDR=8500 -p 8085:8085 -p 8500:8500 golang:latest
 
 */
